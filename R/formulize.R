d416479297ee0819dd8e47eed9a1ac3d6aabcd9d
Alex Hayes <alexpghayes@gmail.com>
2018-01-09 17:23:30 +0000

    version 0.1.0

---
+++ R/formulize.R
@@ -0,0 +1,128 @@
+get_design <- function(design, data, ...) {
+  UseMethod("get_design")
+}
+
+get_design.formula <- function(formula, data, ...) {
+
+  y <- stats::model.response(stats::model.frame(formula, data))
+  X <- stats::model.matrix(formula, data)
+
+  assign <- attr(X, "assign")
+  intercept <- 0 %in% assign        # check if formula included intercept term
+
+  if (intercept && length(assign) == 1) {
+    warning("Intercept only models may break things.")
+  }
+
+  list(X = X, y = y, intercept = intercept)
+}
+
+#' @importFrom recipes all_predictors all_outcomes
+get_design.recipe <- function(recipe, data, ...) {
+
+  prepped <- recipes::prep(recipe)
+  X <- recipes::bake(prepped, data, all_predictors())
+  y <- recipes::bake(prepped, data, all_outcomes())
+
+  X <- as.matrix(X)
+  y <- as.matrix(y)
+  y <- if (ncol(y) == 1) as.vector(y) else y
+
+  # TODO: update to detect_step after PR gets merged
+  intercept <- "step_intercept" %in% unlist(lapply(prepped$steps, class))
+
+  list(X = X, y = y, intercept = intercept)
+}
+
+#' Predict on new data form a \code{wrapped} object
+#'
+#' @param object Object of class \code{wrapped} with a subclass with a
+#'   valid \code{predict} method.
+#' @param newdata Data to predict on.
+#' @param ... Additional arguments to pass to the subclass predict method.
+#'
+#' @return Predictions from subclass predict method.
+#' @export
+#' @importFrom recipes all_predictors
+predict.wrapped <- function(object, newdata = NULL, ...) {
+
+  des <- object$design
+
+  if (inherits(des, "formula")) {
+    newdata <- stats::model.matrix(des, newdata)
+  } else {
+    newdata <- as.matrix(recipes::bake(des, newdata, all_predictors()))
+  }
+  NextMethod()
+}
+
+#' Create a formula/recipe interface to a modelling method
+#'
+#' @param f A function with a matrix/vector interface. Assumes data is passed
+#'   to this function via some combination of arguments \code{x}, \code{X},
+#'   \code{y}, \code{Y}
+#'
+#' @return The same function, wrapped to have formula/dataframe and
+#'   recipe/dataframe interfaces.
+#' @export
+#'
+#' @examples
+#'
+#' library(glmnet)
+#'
+#' glmnet_form <- formulize(cv.glmnet)
+#'
+#' model <- glmnet_form(mpg ~ drat * hp - 1, mtcars)
+#' predict(model, head(mtcars))
+#'
+formulize <- function(f) {
+  function(design, data, ...) {
+
+    des <- get_design(design, data)
+
+    # the following can break if functions have both `x` and `X` as arguments
+    x_arg <- match.arg(c("x", "X"), names(formals(f)), several.ok = TRUE)
+    y_arg <- match.arg(c("y", "Y"), names(formals(f)), several.ok = TRUE)
+
+    args <- list(des$X, des$y)
+    names(args) <- c(x_arg, y_arg)
+    object <- do.call("f", args)
+    object$design <- design
+    class(object) <- c("wrapped", class(object))
+    object
+  }
+}
+
+#' Turn a modelling function into an S3 modelling generic
+#'
+#' Call this function for its side effects: (1) creating an S3 generic of the
+#' passed modelling function, (2) creating a default method that's exactly the
+#' X/y interface to formulize, (3) creating a formula method with formulize.
+#'
+#' @param f Modelling function to turn into a generic.
+#' @param fname Optional name of new generic. Useful because otherwise the
+#'   created generic masks the original modelling function.
+#'
+#' @return Nothing.
+#' @export
+genericize <- function(f, fname = NULL) {
+
+  fname <- if (is.null(fname)) as.character(substitute(f)) else fname
+
+  if ("::" %in% fname || ":::" %in% fname)
+    stop("Do not use genericize with double or triple colon operators.")
+
+  if (utils::isS3stdGeneric(f))
+    stop(paste("S3 generic already exists for this function."))
+
+  env <- globalenv()
+  gen <- rlang::expr_interp(function(obj, ...) UseMethod(!!fname))
+  environment(gen) <- env
+  wrapper <- formulize(f)
+
+  assign(fname, gen, envir = env)
+  assign(paste0(fname, ".default"), f, envir = env)
+  assign(paste0(fname, ".formula"), wrapper, envir = env)
+  assign(paste0(fname, ".recipe"), wrapper, envir = env)
+}
+
---
+++ README.md
@@ -0,0 +1,114 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+formulize
+=========
+
+[![Travis build status](https://travis-ci.org/alexpghayes/formulize.svg?branch=master)](https://travis-ci.org/alexpghayes/formulize) [![AppVeyor Build Status](https://ci.appveyor.com/api/projects/status/github/alexpghayes/formulize?branch=master&svg=true)](https://ci.appveyor.com/project/alexpghayes/formulize) [![Coverage status](https://codecov.io/gh/alexpghayes/formulize/branch/master/graph/badge.svg)](https://codecov.io/github/alexpghayes/formulize?branch=master)
+
+If you:
+
+-   like using formulas, recipes and data frames to specify design matrices
+-   develop nervous ticks when you come across modelling packages that only offer matrix/vector interfaces
+-   don't have the time or motivation to write a formula wrapper around these interfaces
+-   like untested and hacky software written by amateurs
+
+then `formulize` may be for you. Formulize is very new, but you can still install formulize from github with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("alexpghayes/formulize")
+```
+
+Adding a formula or recipe interface
+------------------------------------
+
+Suppose you want to add a formula interface to an existing modelling function, say `cv.glmnet`. Then you could do the following
+
+``` r
+library(recipes)
+library(glmnet)
+library(formulize)
+
+glmnet_cv <- formulize(cv.glmnet)
+
+glmnet_model <- glmnet_cv(mpg ~ drat + hp - 1, mtcars)
+predict(glmnet_model, head(mtcars))
+#>                          1
+#> Mazda RX4         22.35385
+#> Mazda RX4 Wag     22.35385
+#> Datsun 710        22.85056
+#> Hornet 4 Drive    19.97909
+#> Hornet Sportabout 17.72895
+#> Valiant           19.24104
+```
+
+Similarly `glmnet_cv` works with recipe objects like so
+
+``` r
+rec <- recipe(mpg ~ drat + hp, data = mtcars)
+
+glmnet_model2 <- glmnet_cv(rec, mtcars)
+predict(glmnet_model2, head(mtcars))
+#>             1
+#> [1,] 22.35392
+#> [2,] 22.35392
+#> [3,] 22.85062
+#> [4,] 19.97897
+#> [5,] 17.72884
+#> [6,] 19.24084
+```
+
+You may also be interested in the more ~~dangerous~~ exciting version `genericize`, which you should call for its side effects.
+
+``` r
+genericize(cv.glmnet)
+
+form <- mpg ~ drat + hp - 1
+X <- model.matrix(form, mtcars)
+y <- mtcars$mpg
+
+set.seed(27)
+mat_model <- cv.glmnet(X, y, intercept = TRUE)
+
+set.seed(27)
+frm_model <- cv.glmnet(form, mtcars, intercept = TRUE)
+
+set.seed(27)
+rec_model <- cv.glmnet(rec, mtcars, intercept = TRUE)
+
+predict(mat_model, head(X))
+#>                          1
+#> Mazda RX4         22.25028
+#> Mazda RX4 Wag     22.25028
+#> Datsun 710        22.73249
+#> Hornet 4 Drive    20.01959
+#> Hornet Sportabout 17.84620
+#> Valiant           19.33092
+predict(frm_model, head(mtcars))
+#>                          1
+#> Mazda RX4         22.25028
+#> Mazda RX4 Wag     22.25028
+#> Datsun 710        22.73249
+#> Hornet 4 Drive    20.01959
+#> Hornet Sportabout 17.84620
+#> Valiant           19.33092
+predict(rec_model, head(mtcars))
+#>             1
+#> [1,] 22.25035
+#> [2,] 22.25035
+#> [3,] 22.73255
+#> [4,] 20.01946
+#> [5,] 17.84608
+#> [6,] 19.33070
+```
+
+This creates a new S3 generic `cv.glmnet`, sets the provided function as the default method (`cv.glmnet.default`), and adds methods `cv.glmnet.formula` and `cv.glmnet.recipe` using `formulize`.
+
+This will mask `cv.glmnet` and features no safety checks because safety isn't fun.
+
+Caveats
+-------
+
+-   `formulize` doesn't do anything special with intercepts. This means that you need to careful with functions that require you to specify intercepts in non-standard ways, such as `cv.glmnet` above.
+-   If the original modelling function doesn't return a list, `formulize` will probably break.
+-   If you're just looking for a formula interface to `glmnet`, take a look at [glmnetUtils](https://github.com/Hong-Revo/glmnetUtils).
